d0556042d2508b866664a04e2665f8ae1e15dac1
alan-neurpoint-ai <alan@neuropoint.ai>
2026-05-01 20:51:27 -0600

    estilo: implementar componente Button con variantes y pruebas asociadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { Button } from "./presentation/components/ui/Button/Button";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex flex-col sm:flex-row gap-4 p-8">
+        <Button variant="login" onClick={() => console.log("Auth")} />
+        <Button variant="delete">Eliminar Nodo</Button>
+        <Button variant="cancel" className="opacity-80" />
+      </div>
+    </div>
+  );
 }
