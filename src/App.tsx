317f879a2448fe969aef75353407d7db278c7741
alan-neurpoint-ai <alan@neuropoint.ai>
2026-05-01 21:08:12 -0600

    estilo: implementar componente Input con validaciones y pruebas asociadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import { Button } from "./presentation/components/ui/Button/Button";
-
 export default function App() {
-  return (
-    <div>
-      <div className="flex flex-col sm:flex-row gap-4 p-8">
-        <Button variant="login" onClick={() => console.log("Auth")} />
-        <Button variant="delete">Eliminar Nodo</Button>
-        <Button variant="cancel" className="opacity-80" />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
