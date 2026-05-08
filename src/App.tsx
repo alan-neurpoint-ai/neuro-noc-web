2d6d5de6af15d5a4aac59f291605699fee7a25f5
wasakabeofficial <wasakabeofficial@gmail.com>
2026-05-08 12:13:28 -0600

    refactor: mejorar la estructura del componente App y ajustar la importación de tailwindcss en index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import { AppRouter } from "./main/routes/AppRouter";
+import { useAuthStore } from "./modules/auth/presentation/stores/useAuthStore";
+import { authService } from "./modules/auth/infrastructure/services/auth.service";
+
 export default function App() {
-  return <div></div>;
+  const setAuth = useAuthStore((state) => state.setAuth);
+  useEffect(() => {
+    authService.getCurrentSession().then((data) => {
+      if (data?.profile) setAuth(data.profile);
+      else useAuthStore.getState().setAuth(null);
+    });
+  }, [setAuth]);
+
+  return <AppRouter />;
 }
